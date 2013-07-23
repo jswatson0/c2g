b98f6eb2221ac6bb7fed44a97abb204ce8084080
Taylor Donner <atdonner@gmail.com>
2013-07-23 14:13:52 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marketplace::Application.config.secret_token = 'f17f54bfbc089f979cd18208017389a7e6dfbad633d63c84f5606fab368ee582714e6f1ce44db7b6f0c32ede3756d83d39f6c09a315ee9773907ac3806edf419'
